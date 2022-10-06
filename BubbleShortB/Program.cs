1d836856413449eb60d75b3f68ce2d1550f6dee7
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2022-10-06 16:45:21 +0700

    Add project files.

---
+++ BubbleShortB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BubbleShortB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
